a63209750cbd670fe217cb13e38eb39a7ead72e1
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-10-21 19:51:25 +0200

    animations loading from xml

--- King_Game/Motor2D/j1Map.cpp
+++ King_Game/Motor2D/j1Map.cpp
@@ -98,7 +98,7 @@ bool j1Map::CleanUp()
 		LOG("Objectgroups releasing");
 		for (uint i = 0; i < item3->data->size; i++)
 		{
-			while (item3->data->collider[i] != nullptr)
+			while ((item3->data->collider[i] != nullptr)&&(item3->data->collider[i]->to_delete == false))
 			{
 				item3->data->collider[i]->to_delete = true;
 				item3->data->collider[i] = nullptr;
